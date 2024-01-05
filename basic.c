4469ab18166ffadccf1901f2fe7a10f0819bb1b9
PRAN <prantokumar.net@gmail.com>
2024-01-06 01:51:57 +0600

    added basic

---
+++ basic.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+   int main (){
+      FILE *text_new ;
+            text_new = fopen ("text_new.txt" , "r");
+      FILE *text ;//FILE->text.txt
+            text = fopen ("text.txt" , "r"); //pointer file define;
+         // if (text_new == NULL)
+         // {
+         //    printf("No text here");
+         // }
+         // else
+         // {
+         //    fclose(text_new);
+         // }
+         char ch; 
+         fscanf(text_new,"%c",&ch);
+         printf("%c",ch);
+         fscanf(text_new,"%c",&ch);
+         printf("%c",ch);
+         fscanf(text_new,"%c",&ch);
+         printf("%c",ch);
+         fscanf(text_new,"%c",&ch);
+         printf("%c",ch);
+         fscanf(text_new,"%c",&ch);
+         printf("%c",ch);
+         fscanf(text_new,"%c",&ch);
+         printf("%c",ch);
+
+            char ch2;
+            fscanf(text , "%c" , &ch);
+               printf("%c",ch);
+            fscanf(text , "%c" , &ch);
+               printf("%c",ch);
+            fscanf(text , "%c" , &ch);
+               printf("%c",ch);
+            fscanf(text , "%c" , &ch);
+               printf("%c",ch);
+            fscanf(text , "%c" , &ch);
+               printf("%c",ch);
+            fscanf(text , "%c" , &ch);
+               printf("%c",ch);
+   }
